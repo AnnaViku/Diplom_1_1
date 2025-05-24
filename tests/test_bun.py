74743070b89113aba7da43d0557dfcfc01c6b36a
Anna Vikulova <annie.vikulova@mail.ru>
2025-05-24 14:02:47 +0300

    Commit 3

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,12 +1,13 @@
-from praktikum.bun import Bun
 import pytest
+from praktikum.bun import Bun
 
-@pytest.mark.parametrize("name, price", [
-    ("black bun", 100),
-    ("white bun", 200),
-    ("red bun", 300),
-])
-def test_bun_getters(name, price):
-    bun = Bun(name, price)
+@pytest.mark.parametrize("name", ["black bun", "white bun", "red bun"])
+def test_bun_get_name(name):
+    bun = Bun(name, 100)
     assert bun.get_name() == name
+
+@pytest.mark.parametrize("price", [100, 200, 300])
+def test_bun_get_price(price):
+    bun = Bun("Test Bun", price)
     assert bun.get_price() == price
+
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,13 +1,19 @@
+import pytest
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
-import pytest
 
-@pytest.mark.parametrize("type_, name, price", [
-    (INGREDIENT_TYPE_SAUCE, "hot sauce", 100),
-    (INGREDIENT_TYPE_FILLING, "cutlet", 250),
-])
-def test_ingredient_getters(type_, name, price):
-    ingredient = Ingredient(type_, name, price)
+@pytest.mark.parametrize("type_", [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
+def test_ingredient_get_type(type_):
+    ingredient = Ingredient(type_, "Test", 100)
     assert ingredient.get_type() == type_
+
+@pytest.mark.parametrize("name", ["hot sauce", "cutlet"])
+def test_ingredient_get_name(name):
+    ingredient = Ingredient(INGREDIENT_TYPE_FILLING, name, 100)
     assert ingredient.get_name() == name
+
+@pytest.mark.parametrize("price", [100, 250])
+def test_ingredient_get_price(price):
+    ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Test", price)
     assert ingredient.get_price() == price
+
